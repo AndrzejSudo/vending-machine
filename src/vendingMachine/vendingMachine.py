66f2552730e7580adc135da01ba246936e31cb5c
andsko92 <jk2rlz@gmail.com>
2021-10-14 00:29:53 +0200

    Further updates

--- src/vendingMachine/vendingMachine.py
+++ src/vendingMachine/vendingMachine.py
@@ -1,9 +1,48 @@
-import sys, openpyxl, os
+"""Python vending machine"""
 
-print('openpyxl imported successfully')
+import random, sys
 
+try:
+    import openpyxl
+except ModuleNotFoundError:
+    print('This program requires openpyxl module, install it with pip')
+    sys.exit()
 
-def getSnacks(): #extracting vending content to arrays
+# this vending machine is going to use Polish national currency called "Polish zloty" (code: PLN)
+# available coin nominals are 1gr, 2gr, 5gr, 10gr, 20gr, 50gr, 1zl, 2zl, 5zl
+# this vending machine wont accept coins with denominations smaller than 10gr
+COINS = [0.1, 0.2, 0.5, 1, 2, 5]
+
+def main():
+    
+    print('Welcome to vending machine.')
+    print('Pick snacks using their corresponding numbers.\nWe have:')
+    snacks = getSnacks()[1].keys()
+    for index, item in enumerate(snacks):
+        print(index,':', item, end=' | ')
+    print('')
+
+    wallet = 20
+        
+    while True:
+        
+        if wallet == 0:
+            print('You are out of money')
+            sys.exit()
+        
+        print('What do you want to buy?')
+        print(f'You have {WALLET}PLN left')
+        echo = input('> ')
+
+        if echo.isdecimal() and 0 <= int(echo) <= len(snacks)-1:
+            snack = int(echo)
+            break
+        else:
+            print('Invalid input, try again')
+        
+    order = buySnacks(snack, wallet)
+
+def getSnacks():
 
     try:
         snacks_wb = openpyxl.load_workbook('snacks.xlsx')
@@ -12,18 +51,19 @@ def getSnacks(): #extracting vending content to arrays
         sys.exit()
     
     snacks_sh = snacks_wb['Sheet1']
-    snacks = {}
-    quantity = {}
+    snacks_price = {}
+    snacks_quantity = {}
 
-    for snack_name, snack_price in zip(snacks_sh['A'], snacks_sh['B']):
-        snacks[snack_name.value] = snack_price.value
+    for name, price, quantity in zip(snacks_sh['A'], snacks_sh['B'], snacks_sh['C']):
+        snacks_price[name.value] = price.value
+        snacks_quantity[name.value] = quantity.value
+        
+    return snacks_price, snacks_quantity
+
+def buySnacks(purchase, wallet):
+
+
+
+
+main()
     
-    for snack_name, snack_quantity in zip(snacks_sh['A'], snacks_sh['C']):
-        quantity[snack_name.value] = snack_quantity.value
-    
-    return snacks, quantity
-
-print(getSnacks()[1])
-
-
-
