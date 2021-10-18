90f970da1eb6cee950814c4d0dabf0f283e06836
andsko92 <jk2rlz@gmail.com>
2021-10-19 00:34:00 +0200

    Updating with more functionalities

--- src/vendingMachine/vendingMachine.py
+++ src/vendingMachine/vendingMachine.py
@@ -13,17 +13,17 @@ except ModuleNotFoundError:
 # this vending machine wont accept coins with denominations smaller than 10gr
 COINS = [0.1, 0.2, 0.5, 1, 2, 5]
 WALLET = 20
+bought_snacks = []
 
 print('Welcome to vending machine.')
 
 def main():
     
-    print('Purchase snacks using their corresponding numbers.\nWe have:')
     snacks_pc, snacks_qt = getSnacks()[0], getSnacks()[1]
     snacks = snacks_pc.keys()
     snacks_list = []
-    bought_snacks = []
 
+    print('Purchase snacks using their corresponding numbers:')
     for index, item in enumerate(snacks):
         snacks_list.append(item)
         print(index,':', item, end=' | ')
@@ -47,7 +47,7 @@ def main():
             elif response == 2:
                 getRefund(bought_snacks, snacks, snacks_list, snacks_pc, snacks_qt)
             elif response == 3:
-                serviceMode()
+                serviceMode(snacks_pc)
             else:
                 print('Goodbye, have a nice day')
                 sys.exit()
@@ -59,7 +59,7 @@ def main():
 def getSnacks():
 
     try:
-        snacks_wb = openpyxl.load_workbook('snacks.xlsx')
+        snacks_wb = openpyxl.load_workbook('snacks.xlsx', data_only=True)
     except FileNotFoundError:
         print('Snacks spreadsheet not found, ensure its in the same directory as program')
         sys.exit()
@@ -71,7 +71,9 @@ def getSnacks():
     for name, price, quantity in zip(snacks_sh['A'], snacks_sh['B'], snacks_sh['C']):
         snacks_price[name.value] = price.value
         snacks_quantity[name.value] = quantity.value
-        
+    
+    snacks_wb.close()
+
     return snacks_price, snacks_quantity
 
 def pickSnack(bought_snacks, snacks, snacks_pc, snacks_qt, snacks_list):
@@ -114,9 +116,9 @@ def buySnacks(bought_snacks, snack_id, snack_name, snack_pc, snacks_qt):
     pay = 0
 
     while pay != snack_pc:
-
         print(f'{round(snack_pc-pay, 2)} PLN left to pay')
         amount = input('> ')
+
         if amount.upper().startswith('C'):
             print('Canceling purchase')
             WALLET += pay
@@ -133,7 +135,6 @@ def buySnacks(bought_snacks, snack_id, snack_name, snack_pc, snacks_qt):
             WALLET -= amount
             if pay < snack_pc:
                 continue
-            
             else:
                 change = round(pay-snack_pc, 2)
                 print(f'You\'ve inserted {change} PLN too much.\nReturning change.')
@@ -148,9 +149,9 @@ def buySnacks(bought_snacks, snack_id, snack_name, snack_pc, snacks_qt):
     bought_snacks += [snack_id]
 
     print('Purchase confirmed.\nPlease hold', end=' ')
-    for dot in range(3):
-        time.sleep(1)
-        print('.', end=' ')
+    # for dot in range(3):
+        # time.sleep(1)
+        # print('.', end=' ')
     print('\nHere is your snack!')
     print(f'****{snack_name.upper()}****')
     return WALLET
@@ -166,6 +167,7 @@ def getRefund(bought_snacks, snacks, snacks_list, snacks_pc, snacks_qt):
 
     while True:
         refunded_snack = input('> ')
+
         if refunded_snack.isdecimal():
             if int(refunded_snack) in bought_snacks:
                 refunded_snack = int(refunded_snack)
@@ -176,6 +178,7 @@ def getRefund(bought_snacks, snacks, snacks_list, snacks_pc, snacks_qt):
                     print(f'{snacks_list[id]}, ', end=' ')
                 print('\nPick again')
                 continue
+
             else:
                 print('There is no such snack under this number.\nTry again.')
                 continue 
@@ -187,6 +190,7 @@ def getRefund(bought_snacks, snacks, snacks_list, snacks_pc, snacks_qt):
     for dot in range(3):
         time.sleep(1)
         print('.', end=' ')
+
     print(f'\nYou\'ve returned {snacks_list[refunded_snack]}')
     snack_pc = float(snacks_pc[snacks_list[refunded_snack]])
     WALLET += snack_pc
@@ -198,12 +202,104 @@ def getRefund(bought_snacks, snacks, snacks_list, snacks_pc, snacks_qt):
         while True:
             response = input('> ')
             if response.upper().startswith('Y'):
-                getRefund()
+                getRefund(bought_snacks, snacks, snacks_list, snacks_pc, snacks_qt)
+            elif response.upper().startswith('N'):
+                main()
+            else:
+                print('Unrecognized input, try again.')
     else:
         main()
 
-def serviceMode():
-    print('*****SERVICE MODE*****')
+def serviceMode(snacks_price):
+    
+    while True:
+        print('*****SERVICE MODE*****')
+        print('''In service mode you can:
+    1. Increase your wallet ballance
+    2. Change snacks prices
+    3. Change snacks quantities
+    4. Add new snacks
+    5. Exit service mode
+************************''')
+        mode = input('> ')
+
+        if mode.isdecimal() and int(mode) in range(6):
+            mode = int(mode)
+            if mode == 1:
+                print('Enter how much PLN would you like to add to your wallet')
+                while True:
+                    try:
+                        amount = float(input('> '))
+                    except ValueError:
+                        print('Invalid amount, try again')
+                        continue
+                    if float(amount) > 0:
+                        global WALLET
+                        WALLET += float(amount)
+                        print(f'New wallet ballance: {WALLET}')
+                        break
+                    else:
+                        print('Added amount, can\'t be negative')
+                continue
+
+            elif mode == 2:
+                snacks_wb = openpyxl.load_workbook('snacks.xlsx')
+                snacks_sh = snacks_wb['Sheet1']
+                snacks_list = []
+
+                counter = 0
+                print(f'Currently there are {len(snacks_price)} snacks, priced as follows:')
+                for name, price in snacks_price.items():
+                    print(f'{counter}. {name}: {price}; ', end=' ')
+                    counter += 1
+                for item in snacks_price.keys():
+                    snacks_list.append(item)
+                print('')
+                    
+                while True:
+                    print('For what snack would you like to change the price?')
+                    item_id = input('> ')
+                    if item_id.isdecimal() and int(item_id) in range(len(snacks_list)+1):
+                        item_id = int(item_id)
+                        break
+                    else:
+                        print('Invalid input, try again')
+
+                print(f'You\'ve picked {snacks_list[item_id]} which currently costs {snacks_price[snacks_list[item_id]]} PLN')
+                while True:
+                    print(f'What will be new price for {snacks_list[item_id]}?')
+                    try:
+                        price = float(input('> '))
+                        if price > 0:
+                            break
+                        else:
+                            print('Price can\'t be negative')
+                    except ValueError:
+                        print('Invalid price, try again')
+                        continue
+
+                snacks_sh[f'B{item_id+1}'] = price             
+                snacks_wb.save('snacks.xlsx')
+                snacks_wb.close()
+                print('Price changed')
+                continue
+
+            elif mode == 3:
+                main()
+            elif mode == 4:
+                main()
+            elif mode == 5:
+                print('Quitting service mode')
+                for dot in range(3):
+                    time.sleep(1)
+                    print('.', end=' ')
+                print('')
+                main()
+
+        else:
+            print('Invalid input, pick proper mode number.')
+            
+
     main()
 
 if __name__ == '__main__':
